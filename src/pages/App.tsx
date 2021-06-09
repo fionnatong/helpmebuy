8670422e4fcbcc00474a6f802b94bd76b0bd8011
fionna <fionnatong@gmail.com>
2021-06-09 20:48:42 +0800

    chore: set up styled-components

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import { Wrapper, Header } from "./styles";
+
+const App = (): JSX.Element => {
+  return (
+    <Wrapper>
+      <Header>
+        <h1>helpmebuy!</h1>
+      </Header>
+    </Wrapper>
+  );
+}
+
+export default App;
